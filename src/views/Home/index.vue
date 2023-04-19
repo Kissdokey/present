c3cc48c3c00bfe9e7a62ed777e7b8f5420387201
lijian <2779653684@qq.com>
2023-04-19 18:36:46 +0800

    滚轮左右

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,89 +1,174 @@
+<style>
+ 
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+ 
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+ 
+ 
+  .lateral-sliding {
+    display: flex;
+    overflow-y: hidden;
+    overflow-x: scroll;
+  }
+ 
+  .lateral-sliding-item {
+    display: flex;
+    margin-right: 8px;
+    background-color: yellow;
+  }
+ 
+  .each-img {
+    min-width: 350px;
+    min-height: 758px;
+  }
+  #jjListDiv {
+   position:sticky;
+   top:0
+  }
+ 
+</style>
+ 
 <template>
-  <div >
-    <div class="loading-mask" v-if="isLoad">
-      <div class="keyword-box" >
-        <p>111</p>
-        <p>222</p>
-        <p>333</p>
-        <p>444</p>
-      </div>
-    </div>
-    <div>636e9
-        <ul>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-        </ul>
-        <ul id="vertical">
-            <li>12</li>
-            <li>12</li>
-            <li>12</li>
-            <li>12</li>
-            <li>12</li>
-            <li>12</li>
-            <li>12</li>
-            <li>12</li>
-            <li>12</li>
-        </ul>
-        <ul>
-            <li>2</li>
-            <li>2</li>
-            <li>2</li>
-            <li>2</li>
-            <li>2</li>
-            <li>2</li>
-            <li>2</li>
-            <li>2</li>
-            <li>2</li>
-        </ul>
-    </div>
+  <div class="app-container">
+    <ul>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+          </ul>
+    <el-row :gutter="15" type="flex" justify="start" >
+      <el-col :span="20">
+        <div id="jjListDiv" class="grid-content bg-purple-light lateral-sliding">
+          <div class="lateral-sliding-item" v-for="(item,index) in 9" :key="index">
+            <div class="each-img">{{index}}</div>
+          </div>
+        </div>
+      </el-col>
+    </el-row>
+    <ul>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+          </ul>
   </div>
 </template>
 <script>
-export default {
-  name: "HomePage",
-  data() {
-    return {
-      isLoad: true,
-      documentObj:null
-    };
-  },
-  created() {
-    let dom = document.querySelector("#live2dcanvas");
-    if (dom) {
-      dom.parentElement.removeChild(dom);
-    }
-    let child = document.getElementById("canvas_sakura");
-    child.parentNode.removeChild(child);
-  },
-  mounted() {
-    this.setScroolFun();
-    setTimeout(() => {
-      this.isLoad = false;
-    }, 2000);
-    
-  },
-  methods: {
-    onclick() {
-      this.$store.commit("logOut");
-      this.$router.push("/login");
-      location.reload();
+
+
+  export default {
+    name: 'listDom',
+    components: {},
+    props: {},
+    data() {
+      return {
+        x:0,
+        y:0,
+        documentObj: null,
+    }},
+    computed: {
+      getCxGrade: function() {
+        return this.ruleForm.cxGrade
+      }
     },
-    setScroolFun() {
+    created() {
+ 
+    },
+ 
+    mounted() {
+      this.$nextTick(()=>{
+        document.addEventListener('mousewheel',()=>{
+        this.documentObj = document.getElementById('jjListDiv')  
+        this.x = this.documentObj.scrollLeft 
+        this.y = this.documentObj.getBoundingClientRect().top
+        console.log(this.y,this.x)
+      })
+      })
+    },
+    beforeDestroy() {
+      if (!this.documentObj) return
+      this.documentObj.removeEventListener('DOMMouseScroll', this.handlerMouserScroll)
+      this.documentObj.removeEventListener('mousewheel', this.handlerMouserScroll)
+    },
+    watch: {
+      y(y1){
+        if(y1<=0){
+          this.setScroolFun();
+        }
+      },
+      x(x1){
+        if(x1===2364||x1===0){
+          console.log(123)
+          let dom = document.getElementById('jjListDiv')  
+          dom.style.position='relative'
+          this.documentObj.removeEventListener('DOMMouseScroll', this.handlerMouserScroll)
+          this.documentObj.removeEventListener('mousewheel', this.handlerMouserScroll)
+        }
+      }
+    },
+    methods: {
+      /*容器绑定鼠标滚轮事件*/
+      setScroolFun() {
         //绑定的容器
-        this.documentObj = document.getElementById('vertical') // 获取DOM元素节点
-        if(this.documentObj!==null) console.log(111);
+        this.documentObj = document.getElementById('jjListDiv') // 获取DOM元素节点
         // 添加监听事件（不同浏览器，事件方法不一样，所以可以作判断，也可以如下偷懒）
         this.documentObj.addEventListener('DOMMouseScroll', this.handlerMouserScroll, false)//滚轮事件
-        this.documentObj.addEventListener('mousewheel', this.handlerMouserScroll, false)//滚轮事件
+       this.documentObj.addEventListener('mousewheel', this.handlerMouserScroll, false)//滚轮事件
       },
       handlerMouserScroll(event) {
+        event.preventDefault();
+        this.documentObj = document.getElementById('jjListDiv')   
         //获取滚轮跨距，兼容获取方式
         let detail = event.wheelDelta || event.detail || event.wheelDeltaY
         /*反向*/
@@ -116,63 +201,9 @@ export default {
         }, paceNum)
  
  
-      },
-  },
-};
-</script>
-<style scoped>
-.loading-mask {
-    position: fixed;
-    top:0;
-    left:0;
-    width: 100%;
-    height: 100%;
-    background-color: black; 
-}
-.logout-button {
-  position: fixed;
-  right: 30px;
-  bottom: 30px;
-}
-.keyword-box p {
-  opacity: 0;
-  line-height: 6;
-  font-size: x-large;
-  position:absolute;
-  left:50%;
-  top:50%;
-  transform: translate(-50%,-50%);
-  animation: flash 0.5s linear;
-}
-.keyword-box p:nth-child(1) {
-  animation-delay: 0s;
-}
-.keyword-box p:nth-child(2) {
-  animation-delay: 0.5s;
-}
-.keyword-box p:nth-child(3) {
-  animation-delay: 1s;
-}
-.keyword-box p:nth-child(4) {
-  animation-delay: 1.5s;
-}
-@keyframes flash {
-  0%,
-  100% {
-    opacity: 1;
-    color: white;
-  }
-}
-#vertical {
-    display: flex;
-    height: 100%;
-    background-color: red;
-}
+      },}
+ 
 
-li{
-    line-height: 6;
-}
-#vertical li {
-    margin: 200px;
-}
-</style>
+  }
+</script>
+ 
---
+++ src/views/aaa.vue
@@ -0,0 +1,179 @@
+<template>
+    <div >
+      <div class="loading-mask" v-if="isLoad">
+        <div class="keyword-box" >
+          <p>111</p>
+          <p>222</p>
+          <p>333</p>
+          <p>444</p>
+        </div>
+      </div>
+      <div>636e9
+          <ul>
+              <li>1</li>
+              <li>1</li>
+              <li>1</li>
+              <li>1</li>
+              <li>1</li>
+              <li>1</li>
+              <li>1</li>
+              <li>1</li>
+              <li>1</li>
+          </ul>
+          <ul id="vertical">
+              <li>12</li>
+              <li>12</li>
+              <li>12</li>
+              <li>12</li>
+              <li>12</li>
+              <li>12</li>
+              <li>12</li>
+              <li>12</li>
+              <li>12</li>
+          </ul>
+          <ul>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+              <li>2</li>
+          </ul>
+      </div>
+    </div>
+  </template>
+  <script>
+  export default {
+    name: "HomePage",
+    data() {
+      return {
+        isLoad: true,
+        documentObj:null
+      };
+    },
+    created() {
+      let dom = document.querySelector("#live2dcanvas");
+      if (dom) {
+        dom.parentElement.removeChild(dom);
+      }
+      let child = document.getElementById("canvas_sakura");
+      child.parentNode.removeChild(child);
+    },
+    mounted() {
+      this.setScroolFun();
+      setTimeout(() => {
+        this.isLoad = false;
+      }, 2000);
+      
+    },
+    methods: {
+      onclick() {
+        this.$store.commit("logOut");
+        this.$router.push("/login");
+        location.reload();
+      },
+      setScroolFun() {
+          //绑定的容器
+          this.documentObj = document.getElementById('vertical') // 获取DOM元素节点
+          if(this.documentObj!==null) console.log(111);
+          // 添加监听事件（不同浏览器，事件方法不一样，所以可以作判断，也可以如下偷懒）
+          this.documentObj.addEventListener('DOMMouseScroll', this.handlerMouserScroll, false)//滚轮事件
+          this.documentObj.addEventListener('mousewheel', this.handlerMouserScroll, false)//滚轮事件
+        },
+        handlerMouserScroll(event) {
+          //获取滚轮跨距，兼容获取方式
+          let detail = event.wheelDelta || event.detail || event.wheelDeltaY
+          /*反向*/
+          let moveForwardStep = -1
+          /*正向*/
+          let moveBackStep = 1
+          let step = 0
+          //如果跨步大于0，表明正向跨步，将跨步放大100倍，改变滑动速度，如果跨步小于0，表明反向跨步，将跨步放大500倍，改变滑动速度
+          step = detail > 0 ? moveForwardStep * 80 : moveBackStep * 80
+          /*覆盖当前滚动条的位置,单位是像素，叠增或剃减*/
+          // this.documentObj.scrollLeft = this.documentObj.scrollLeft + step
+   
+          //平滑值(越小越平滑越持久，反之，总之。总之，不能小于等于0，不能大于等于1，作者建议值：0.8)
+          let slipNum = 0.8
+          //末尾值（越小，则越平稳，越大，则越仓促，作者建议值：5）
+          let endNum = 5
+          /*递减步伐最大初始值=滚轮单位跨步值*/
+          let decreasingPaceNum = step
+          /*平滑速度，越大，则越慢，越小，则越慢，作者建议值：70*/
+          let paceNum=70;
+   
+          /*效果一*/
+          let t = setInterval(() => {
+            if (Math.abs(decreasingPaceNum) < endNum) {
+              clearInterval(t)
+              return
+            }
+            decreasingPaceNum = decreasingPaceNum * slipNum
+            this.documentObj.scrollLeft = this.documentObj.scrollLeft + decreasingPaceNum
+          }, paceNum)
+   
+   
+        },
+    },
+  };
+  </script>
+  <style scoped>
+  .loading-mask {
+      position: fixed;
+      top:0;
+      left:0;
+      width: 100%;
+      height: 100%;
+      background-color: black; 
+  }
+  .logout-button {
+    position: fixed;
+    right: 30px;
+    bottom: 30px;
+  }
+  .keyword-box p {
+    opacity: 0;
+    line-height: 6;
+    font-size: x-large;
+    position:absolute;
+    left:50%;
+    top:50%;
+    transform: translate(-50%,-50%);
+    animation: flash 0.5s linear;
+  }
+  .keyword-box p:nth-child(1) {
+    animation-delay: 0s;
+  }
+  .keyword-box p:nth-child(2) {
+    animation-delay: 0.5s;
+  }
+  .keyword-box p:nth-child(3) {
+    animation-delay: 1s;
+  }
+  .keyword-box p:nth-child(4) {
+    animation-delay: 1.5s;
+  }
+  @keyframes flash {
+    0%,
+    100% {
+      opacity: 1;
+      color: white;
+    }
+  }
+  #vertical {
+      display: flex;
+      height: 100%;
+      background-color: red;
+  }
+  
+  li{
+      line-height: 6;
+  }
+  #vertical li {
+      margin: 200px;
+  }
+  </style>
+  
